b3c9975000b795ab203e0162ada592abdd18217c
Ankit chaurasiya <iankitkr2005@gmail.com>
2026-01-05 09:45:59 +0530

    Delete Cloudcomputingplatform directory

--- Cloudcomputingplatform/src/utils/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class DBConfig {
-
-    public static final String URL = "jdbc:mysql://localhost:3306/cloud_platform?useSSL=false&serverTimezone=UTC";
-    public static final String USER = "root";          // yaha apna MySQL username
-    public static final String PASSWORD = "iankit2005";  // yaha apna MySQL password
-}
